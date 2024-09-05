53a6aaf3abfee630f1e712f2a88d0adaa1d81131
Jefferson Duarte <duartejefferson123@gmail.com>
2024-09-05 15:20:13 -0300

    feat: crated User class

---
+++ src/main/java/com/jefferson/api_junit_mockito/domain/User.java
@@ -0,0 +1,25 @@
+package com.jefferson.api_junit_mockito.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+}
