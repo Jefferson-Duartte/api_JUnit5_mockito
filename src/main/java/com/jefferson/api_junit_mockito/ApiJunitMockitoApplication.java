897242951d3e69e740c5d5fcb53e649ddf7f8507
Jefferson Duarte <duartejefferson123@gmail.com>
2024-09-05 15:15:25 -0300

    feat: created project

---
+++ src/main/java/com/jefferson/api_junit_mockito/ApiJunitMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.jefferson.api_junit_mockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJunitMockitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiJunitMockitoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jefferson/api_junit_mockito/ApiJunitMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jefferson.api_junit_mockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJunitMockitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
