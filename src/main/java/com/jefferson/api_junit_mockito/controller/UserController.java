da138cb3fd0c82a9e7357f21c9a2167922c4db9a
Jefferson Duarte <duartejefferson123@gmail.com>
2024-09-06 15:52:24 -0300

    feat: mapping user with ModelMapper

--- src/main/java/com/jefferson/api_junit_mockito/controller/UserController.java
+++ src/main/java/com/jefferson/api_junit_mockito/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.jefferson.api_junit_mockito.controller;
 
 import com.jefferson.api_junit_mockito.domain.UserModel;
+import com.jefferson.api_junit_mockito.domain.dto.UserDTO;
 import com.jefferson.api_junit_mockito.service.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,12 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @Autowired
+    private ModelMapper mapper;
+
+    @Autowired
     private UserService service;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<UserModel> getUserById(@PathVariable Long id){
-
-        return ResponseEntity.ok().body(service.findById(id));
+    public ResponseEntity<UserDTO> getUserById(@PathVariable Long id) {
+        return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
     }
 
 }
