c3af7e54e7b798b5bd8dea053cdecb3405a3f3b0
Jefferson Duarte <duartejefferson123@gmail.com>
2024-09-06 01:38:23 -0300

    feat: add Service annotation

--- src/main/java/com/jefferson/api_junit_mockito/services/impl/UserServiceImpl.java
+++ src/main/java/com/jefferson/api_junit_mockito/services/impl/UserServiceImpl.java
@@ -2,7 +2,9 @@ package com.jefferson.api_junit_mockito.services.impl;
 
 import com.jefferson.api_junit_mockito.domain.User;
 import com.jefferson.api_junit_mockito.services.UserService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService {
     @Override
     public User findUserById(Long id) {
