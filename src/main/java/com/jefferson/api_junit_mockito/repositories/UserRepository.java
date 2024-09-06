a55cfeea1f724a886534409d39d571dd0ebd7bd5
Jefferson Duarte <duartejefferson123@gmail.com>
2024-09-06 01:55:07 -0300

    feat: create interface UserRepository

---
+++ src/main/java/com/jefferson/api_junit_mockito/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.jefferson.api_junit_mockito.repositories;
+
+import com.jefferson.api_junit_mockito.domain.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, Long> {
+}
