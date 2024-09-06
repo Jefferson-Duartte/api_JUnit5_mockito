c6a1ffa32f1c2bc251dc5b3ecb9d77450907ce7c
Jefferson Duarte <duartejefferson123@gmail.com>
2024-09-06 01:54:09 -0300

    refactor: rename to UserModel

--- src/main/java/com/jefferson/api_junit_mockito/domain/UserModel.java
+++ src/main/java/com/jefferson/api_junit_mockito/domain/UserModel.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-public class User {
+public class UserModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
