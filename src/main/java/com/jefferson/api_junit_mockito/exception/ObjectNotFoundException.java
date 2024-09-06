b7594ecf3bc34d9b682f2d05e0d6cca91d07ae2b
Jefferson Duarte <duartejefferson123@gmail.com>
2024-09-06 15:13:05 -0300

    feat: create ObjectNotFoundException

---
+++ src/main/java/com/jefferson/api_junit_mockito/exception/ObjectNotFoundException.java
@@ -0,0 +1,7 @@
+package com.jefferson.api_junit_mockito.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+}
