0d0099812b17d9ed2b2d10da4acaff66b8f6788d
Jefferson Duarte <duartejefferson123@gmail.com>
2024-09-06 01:36:19 -0300

    feat: create UserService implementation

---
+++ src/main/java/com/jefferson/api_junit_mockito/services/impl/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package com.jefferson.api_junit_mockito.services.impl;
+
+import com.jefferson.api_junit_mockito.domain.User;
+import com.jefferson.api_junit_mockito.services.UserService;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public User findUserById(Long id) {
+        return null;
+    }
+}
