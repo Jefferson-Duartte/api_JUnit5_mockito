235743ebd725c9ab5444803b29ca005853caaf35
Jefferson Duarte <duartejefferson123@gmail.com>
2024-09-06 01:56:57 -0300

    feat: add repository in UserServiceImpl

--- src/main/java/com/jefferson/api_junit_mockito/services/impl/UserServiceImpl.java
+++ src/main/java/com/jefferson/api_junit_mockito/services/impl/UserServiceImpl.java
@@ -1,13 +1,22 @@
 package com.jefferson.api_junit_mockito.services.impl;
 
-import com.jefferson.api_junit_mockito.domain.User;
+import com.jefferson.api_junit_mockito.domain.UserModel;
+import com.jefferson.api_junit_mockito.repositories.UserRepository;
 import com.jefferson.api_junit_mockito.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository repository;
+
     @Override
-    public User findUserById(Long id) {
-        return null;
+    public UserModel findById(Long id) {
+        Optional<UserModel> user = repository.findById(id);
+        return user.orElse(null);
     }
 }
