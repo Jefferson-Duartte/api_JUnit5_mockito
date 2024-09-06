ee4fc600ce74d1d8598242fe3ed0ddcf988dec5f
Jefferson Duarte <duartejefferson123@gmail.com>
2024-09-06 15:16:49 -0300

    feat: throwing ObjectNotFoundException exception

--- src/main/java/com/jefferson/api_junit_mockito/service/impl/UserServiceImpl.java
+++ src/main/java/com/jefferson/api_junit_mockito/service/impl/UserServiceImpl.java
@@ -1,8 +1,9 @@
-package com.jefferson.api_junit_mockito.services.impl;
+package com.jefferson.api_junit_mockito.service.impl;
 
 import com.jefferson.api_junit_mockito.domain.UserModel;
-import com.jefferson.api_junit_mockito.repositories.UserRepository;
-import com.jefferson.api_junit_mockito.services.UserService;
+import com.jefferson.api_junit_mockito.exception.ObjectNotFoundException;
+import com.jefferson.api_junit_mockito.repository.UserRepository;
+import com.jefferson.api_junit_mockito.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserModel findById(Long id) {
         Optional<UserModel> user = repository.findById(id);
-        return user.orElse(null);
+        return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado."));
     }
 }
